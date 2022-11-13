9ae319b0712b3613add885df65bd1855089a98bb
MetalStormGhost <ghosti@smytky.org>
2022-11-13 19:23:00 +0200

    Increased ctld.JTAC_maxDistance to the previous value it used to have in the legacy JTACAutoLase script.

--- resources/plugins/ctld/CTLD.lua
+++ resources/plugins/ctld/CTLD.lua
@@ -135,7 +135,7 @@ ctld.JTAC_LIMIT_BLUE = 10 -- max number of JTAC Crates for the BLUE Side
 
 ctld.JTAC_dropEnabled = true -- allow JTAC Crate spawn from F10 menu
 
-ctld.JTAC_maxDistance = 10000 -- How far a JTAC can "see" in meters (with Line of Sight)
+ctld.JTAC_maxDistance = 50000 -- How far a JTAC can "see" in meters (with Line of Sight)
 
 ctld.JTAC_smokeOn_RED = true -- enables marking of target with smoke for RED forces
 ctld.JTAC_smokeOn_BLUE = true -- enables marking of target with smoke for BLUE forces

f8c4322880d1618c28f95cecfc4139c88371c9cc
MetalStormGhost <ghosti@smytky.org>
2022-12-07 11:43:03 +0200

    Type conversion (float -> int) in casingress.py altitude handling.

--- game/missiongenerator/aircraft/waypoints/casingress.py
+++ game/missiongenerator/aircraft/waypoints/casingress.py
@@ -68,6 +68,6 @@ class CasIngressBuilder(PydcsWaypointBuilder):
             Bf_109K_4,
             I_16,
         ]:
-            waypoint.alt = waypoint.alt / 3
+            waypoint.alt = int(waypoint.alt / 3)
         else:
-            waypoint.alt = waypoint.alt / 2
+            waypoint.alt = int(waypoint.alt / 2)

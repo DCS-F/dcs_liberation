3568ee350b3367edd2c41aba92805af17ec4961b
MetalStormGhost <ghosti@smytky.org>
2023-01-14 13:56:05 +0200

    Added AIM-9M and AIM-120B loadouts as 1990s loadouts.

---
+++ resources/customized_payloads_1990s/VSN_F16CBL50.lua
@@ -0,0 +1,187 @@
+local unitPayloads = {
+	["name"] = "VSN_F16CBL50",
+	["payloads"] = {
+		[1] = {
+			["displayName"] = "Liberation BARCAP",
+			["name"] = "Liberation BARCAP",
+			["pylons"] = {
+				[1] = {
+					["CLSID"] = "{C8E06185-7CD6-4C90-959F-044679E90751}",
+					["num"] = 1,
+				},
+				[2] = {
+					["CLSID"] = "{C8E06185-7CD6-4C90-959F-044679E90751}",
+					["num"] = 3,
+				},
+				[3] = {
+					["CLSID"] = "{6CEB49FC-DED8-4DED-B053-E1F033FF72D3}",
+					["num"] = 4,
+				},
+				[4] = {
+					["CLSID"] = "{C8E06185-7CD6-4C90-959F-044679E90751}",
+					["num"] = 11,
+				},
+				[5] = {
+					["CLSID"] = "{VSN_F16CCFT_PTB}",
+					["num"] = 6,
+				},
+				[6] = {
+					["CLSID"] = "{C8E06185-7CD6-4C90-959F-044679E90751}",
+					["num"] = 9,
+				},
+				[7] = {
+					["CLSID"] = "{6CEB49FC-DED8-4DED-B053-E1F033FF72D3}",
+					["num"] = 8,
+				},
+				[8] = {
+					["CLSID"] = "{VSN_F16C_PTB}",
+					["num"] = 10,
+				},
+				[9] = {
+					["CLSID"] = "{VSN_F16C_PTB}",
+					["num"] = 2,
+				},
+			},
+			["tasks"] = {
+				[1] = 11,
+			},
+		},
+		[2] = {
+			["displayName"] = "Liberation TARCAP",
+			["name"] = "Liberation TARCAP",
+			["pylons"] = {
+				[1] = {
+					["CLSID"] = "{C8E06185-7CD6-4C90-959F-044679E90751}",
+					["num"] = 1,
+				},
+				[2] = {
+					["CLSID"] = "{C8E06185-7CD6-4C90-959F-044679E90751}",
+					["num"] = 3,
+				},
+				[3] = {
+					["CLSID"] = "{6CEB49FC-DED8-4DED-B053-E1F033FF72D3}",
+					["num"] = 4,
+				},
+				[4] = {
+					["CLSID"] = "{C8E06185-7CD6-4C90-959F-044679E90751}",
+					["num"] = 11,
+				},
+				[5] = {
+					["CLSID"] = "{VSN_F16CCFT_PTB}",
+					["num"] = 6,
+				},
+				[6] = {
+					["CLSID"] = "{C8E06185-7CD6-4C90-959F-044679E90751}",
+					["num"] = 9,
+				},
+				[7] = {
+					["CLSID"] = "{6CEB49FC-DED8-4DED-B053-E1F033FF72D3}",
+					["num"] = 8,
+				},
+				[8] = {
+					["CLSID"] = "{VSN_F16C_PTB}",
+					["num"] = 10,
+				},
+				[9] = {
+					["CLSID"] = "{VSN_F16C_PTB}",
+					["num"] = 2,
+				},
+			},
+			["tasks"] = {
+				[1] = 11,
+			},
+		},
+		[3] = {
+			["displayName"] = "Liberation Escort",
+			["name"] = "Liberation Escort",
+			["pylons"] = {
+				[1] = {
+					["CLSID"] = "{C8E06185-7CD6-4C90-959F-044679E90751}",
+					["num"] = 1,
+				},
+				[2] = {
+					["CLSID"] = "{C8E06185-7CD6-4C90-959F-044679E90751}",
+					["num"] = 3,
+				},
+				[3] = {
+					["CLSID"] = "{6CEB49FC-DED8-4DED-B053-E1F033FF72D3}",
+					["num"] = 4,
+				},
+				[4] = {
+					["CLSID"] = "{C8E06185-7CD6-4C90-959F-044679E90751}",
+					["num"] = 11,
+				},
+				[5] = {
+					["CLSID"] = "{VSN_F16CCFT_PTB}",
+					["num"] = 6,
+				},
+				[6] = {
+					["CLSID"] = "{C8E06185-7CD6-4C90-959F-044679E90751}",
+					["num"] = 9,
+				},
+				[7] = {
+					["CLSID"] = "{6CEB49FC-DED8-4DED-B053-E1F033FF72D3}",
+					["num"] = 8,
+				},
+				[8] = {
+					["CLSID"] = "{VSN_F16C_PTB}",
+					["num"] = 10,
+				},
+				[9] = {
+					["CLSID"] = "{VSN_F16C_PTB}",
+					["num"] = 2,
+				},
+			},
+			["tasks"] = {
+				[1] = 11,
+			},
+		},
+		[4] = {
+			["displayName"] = "Liberation Fighter Sweep",
+			["name"] = "Liberation Fighter Sweep",
+			["pylons"] = {
+				[1] = {
+					["CLSID"] = "{C8E06185-7CD6-4C90-959F-044679E90751}",
+					["num"] = 1,
+				},
+				[2] = {
+					["CLSID"] = "{C8E06185-7CD6-4C90-959F-044679E90751}",
+					["num"] = 3,
+				},
+				[3] = {
+					["CLSID"] = "{6CEB49FC-DED8-4DED-B053-E1F033FF72D3}",
+					["num"] = 4,
+				},
+				[4] = {
+					["CLSID"] = "{C8E06185-7CD6-4C90-959F-044679E90751}",
+					["num"] = 11,
+				},
+				[5] = {
+					["CLSID"] = "{VSN_F16CCFT_PTB}",
+					["num"] = 6,
+				},
+				[6] = {
+					["CLSID"] = "{C8E06185-7CD6-4C90-959F-044679E90751}",
+					["num"] = 9,
+				},
+				[7] = {
+					["CLSID"] = "{6CEB49FC-DED8-4DED-B053-E1F033FF72D3}",
+					["num"] = 8,
+				},
+				[8] = {
+					["CLSID"] = "{VSN_F16C_PTB}",
+					["num"] = 10,
+				},
+				[9] = {
+					["CLSID"] = "{VSN_F16C_PTB}",
+					["num"] = 2,
+				},
+			},
+			["tasks"] = {
+				[1] = 11,
+			},
+		},
+	},
+	["unitType"] = "VSN_F16CBL50",
+}
+return unitPayloads

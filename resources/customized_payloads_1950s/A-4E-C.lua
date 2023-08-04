34f28be0531fb08e9a818a33440c95b6e8b3a799
MetalStormGhost <ghosti@smytky.org>
2023-08-04 18:33:26 +0300

    Added separate 1950s loadouts for the A-4 Skyhawk.

---
+++ resources/customized_payloads_1950s/A-4E-C.lua
@@ -0,0 +1,206 @@
+local unitPayloads = {
+	["name"] = "A-4E-C",
+	["payloads"] = {
+		[1] = {
+			["name"] = "CAP",
+			["pylons"] = {
+				[1] = {
+					["CLSID"] = "{GAR-8}",
+					["num"] = 1,
+				},
+				[2] = {
+					["CLSID"] = "{GAR-8}",
+					["num"] = 2,
+				},
+				[3] = {
+					["CLSID"] = "{DFT-400gal}",
+					["num"] = 3,
+				},
+				[4] = {
+					["CLSID"] = "{GAR-8}",
+					["num"] = 4,
+				},
+				[5] = {
+					["CLSID"] = "{GAR-8}",
+					["num"] = 5,
+				},
+			},
+			["tasks"] = {
+				[1] = 31,
+			},
+		},
+		[2] = {
+			["name"] = "CAS",
+			["pylons"] = {
+				[1] = {
+					["CLSID"] = "{F3EFE0AB-E91A-42D8-9CA2-B63C91ED570A}",
+					["num"] = 1,
+				},
+				[2] = {
+					["CLSID"] = "{LAU-3 FFAR Mk5 HEAT_TER_2_L}",
+					["num"] = 2,
+				},
+				[3] = {
+					["CLSID"] = "{DFT-300gal}",
+					["num"] = 3,
+				},
+				[4] = {
+					["CLSID"] = "{LAU-3 FFAR Mk5 HEAT_TER_2_R}",
+					["num"] = 4,
+				},
+				[5] = {
+					["CLSID"] = "{F3EFE0AB-E91A-42D8-9CA2-B63C91ED570A}",
+					["num"] = 5,
+				},
+			},
+			["tasks"] = {
+				[1] = 31,
+			},
+		},
+		[3] = {
+			["name"] = "STRIKE",
+			["pylons"] = {
+				[1] = {
+					["CLSID"] = "{mk77mod1}",
+					["num"] = 1,
+				},
+				[2] = {
+					["CLSID"] = "{mk77mod0}",
+					["num"] = 2,
+				},
+				[3] = {
+					["CLSID"] = "{DFT-400gal}",
+					["num"] = 3,
+				},
+				[4] = {
+					["CLSID"] = "{mk77mod0}",
+					["num"] = 4,
+				},
+				[5] = {
+					["CLSID"] = "{mk77mod1}",
+					["num"] = 5,
+				},
+			},
+			["tasks"] = {
+				[1] = 31,
+			},
+		},
+		[4] = {
+			["name"] = "SEAD",
+			["pylons"] = {
+				[1] = {
+					["CLSID"] = "{F3EFE0AB-E91A-42D8-9CA2-B63C91ED570A}",
+					["num"] = 1,
+				},
+				[2] = {
+					["CLSID"] = "{LAU-10 ZUNI_TER_2_L}",
+					["num"] = 2,
+				},
+				[3] = {
+					["CLSID"] = "{DFT-300gal}",
+					["num"] = 3,
+				},
+				[4] = {
+					["CLSID"] = "{LAU-10 ZUNI_TER_2_R}",
+					["num"] = 4,
+				},
+				[5] = {
+					["CLSID"] = "{F3EFE0AB-E91A-42D8-9CA2-B63C91ED570A}",
+					["num"] = 5,
+				},
+			},
+			["tasks"] = {
+				[1] = 31,
+			},
+		},
+		[5] = {
+			["name"] = "ANTISHIP",
+			["pylons"] = {
+				[1] = {
+					["CLSID"] = "{AGM_45A}",
+					["num"] = 5,
+				},
+				[2] = {
+					["CLSID"] = "{AGM_45A}",
+					["num"] = 4,
+				},
+				[3] = {
+					["CLSID"] = "{AGM_45A}",
+					["num"] = 2,
+				},
+				[4] = {
+					["CLSID"] = "{AGM_45A}",
+					["num"] = 1,
+				},
+				[5] = {
+					["CLSID"] = "{DFT-300gal}",
+					["num"] = 3,
+				},
+			},
+			["tasks"] = {
+				[1] = 31,
+			},
+		},
+		[6] = {
+			["displayName"] = "Liberation SEAD Escort",
+			["name"] = "Liberation SEAD Escort",
+			["pylons"] = {
+				[1] = {
+					["CLSID"] = "{F3EFE0AB-E91A-42D8-9CA2-B63C91ED570A}",
+					["num"] = 1,
+				},
+				[2] = {
+					["CLSID"] = "{LAU-3 FFAR Mk1 HE_TER_2_L}",
+					["num"] = 2,
+				},
+				[3] = {
+					["CLSID"] = "{DFT-300gal}",
+					["num"] = 3,
+				},
+				[4] = {
+					["CLSID"] = "{LAU-3 FFAR Mk1 HE_TER_2_R}",
+					["num"] = 4,
+				},
+				[5] = {
+					["CLSID"] = "{F3EFE0AB-E91A-42D8-9CA2-B63C91ED570A}",
+					["num"] = 5,
+				},
+			},
+			["tasks"] = {
+				[1] = 29,
+			},
+		},
+		[7] = {
+			["name"] = "DEAD",
+			["pylons"] = {
+				[1] = {
+					["CLSID"] = "{mk77mod1}",
+					["num"] = 1,
+				},
+				[2] = {
+					["CLSID"] = "{LAU-10 ZUNI_TER_2_L}",
+					["num"] = 2,
+				},
+				[3] = {
+					["CLSID"] = "{DFT-300gal}",
+					["num"] = 3,
+				},
+				[4] = {
+					["CLSID"] = "{LAU-10 ZUNI_TER_2_R}",
+					["num"] = 4,
+				},
+				[5] = {
+					["CLSID"] = "{mk77mod1}",
+					["num"] = 5,
+				},
+			},
+			["tasks"] = {
+				[1] = 31,
+			},
+		},
+	},
+	["tasks"] = {
+	},
+	["unitType"] = "A-4E-C",
+}
+return unitPayloads
